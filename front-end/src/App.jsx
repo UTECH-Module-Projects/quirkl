58e2c4ba84625809b05b84a26c0a45581a5331a6
Rushawn White <rushawnwhite29@gmail.com>
2024-04-07 10:59:04 -0500

    Created Front-End Folder

---
+++ front-end/src/App.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+import './App.css'
+import {Box} from "@chakra-ui/react";
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <Box>
+
+        </Box>
+    )
+}
+
+export default App
