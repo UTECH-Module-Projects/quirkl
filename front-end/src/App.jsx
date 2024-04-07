a537b4651fa40759f0eccd9d57ad3a11adba7a07
Rushawn White <rushawnwhite29@gmail.com>
2024-04-07 16:32:45 -0500

    Started Layout for Website

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,12 @@
-import {useState} from 'react'
-import './App.css'
-import {Box} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import CodeEditor from "./components/CodeEditor.jsx";
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <Box>
-
+        <Box minH="calc(100vh - 60px)" bg="#0f0a19" color="gray.500" px={6} py={8}> {/* Subtract 60px from 100vh */}
+            <CodeEditor />
         </Box>
-    )
+    );
 }
 
-export default App
+export default App;
---
+++ front-end/src/components/CodeEditor.jsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+import { Box, Flex } from "@chakra-ui/react"; // Import Box and Flex
+import QuirklEditor from './QuirklLanguage.jsx';
+import Output from "./Output.jsx";
+
+const CodeEditor = () => {
+    const editorRef = useRef(null);
+    const [value, setValue] = useState("");
+
+    const onMount = (editor) => {
+        editorRef.current = editor;
+        editor.focus();
+    };
+
+    return (
+        <Flex w="100%" h="100vh">
+            <Box flex="1" w="100%" h="100%" mr={6}>
+                <QuirklEditor
+                    editorRef={editorRef}
+                    onMount={onMount}
+                    onChange={(value) => setValue(value)}
+                    options={{
+                        minimap: {
+                            enabled: false,
+                        },
+                    }}
+                    height="100%"
+                    theme="vs-dark"
+                />
+            </Box>
+            <Box flex="1" w="100%" h="100%">
+                <Output editorRef={editorRef} />
+            </Box>
+        </Flex>
+    );
+};
+
+export default CodeEditor;
